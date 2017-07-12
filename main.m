a168b13f01219448cf6c4d874d08c30686462a39
kitchell <lindseykitchell21@gmail.com>
2017-07-12 14:36:10 -0400

    update

--- main.m
+++ main.m
@@ -4,15 +4,11 @@ switch getenv('ENV')
 case 'IUHPC'
 	disp('loading paths (HPC)')
 	addpath(genpath('/N/u/hayashis/BigRed2/git/jsonlab'))
-	addpath(genpath('/N/u/kitchell/Karst/Applications/iso2mesh'))
 	addpath(genpath('/N/u/hayashis/BigRed2/git/vistasoft'))
-	addpath(genpath('/N/u/hayashis/BigRed2/git/afq'))
 case 'VM'
 	disp('loading paths (VM)')
 	addpath(genpath('/usr/local/jsonlab'))
-	addpath(genpath('/usr/local/iso2mesh'))
 	addpath(genpath('/usr/local/vistasoft'))
-	addpath(genpath('/usr/local/afq'))
 end
 
 % load config.json
